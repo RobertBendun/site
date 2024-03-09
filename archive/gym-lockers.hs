7beda59627b47d9c8957bce1d578b6b765e72c66
Robert Bendun <robert@bendun.cc>
2024-03-10 00:57:36 +0100

    new post: gym lockers

--- archive/gym-lockers.hs
+++ archive/gym-lockers.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Control.Monad
 import Data.Monoid
+import Data.List
+import Data.Bool
+import Text.Printf
 
 (.:) = (.) . (.)
 
@@ -12,11 +15,11 @@ digits :: Int -> [Int]
 digits = map (`mod` 10) . reverse . takeWhile (> 0) . iterate (`div` 10)
 
 foolproofDivisibilityTests, allDivisibilityTests :: [Int]
-foolproofDivisibilityTests = [2,3,5,10]
+foolproofDivisibilityTests = [2,3,5]
 allDivisibilityTests = foolproofDivisibilityTests ++ [11]
 
 isSquare :: Int -> Bool
-isSquare x = let s = floor $ sqrt $ fromIntegral x in s * s == x && s <= 11
+isSquare x = let s = floor $ sqrt $ fromIntegral x in s * s == x && s <= 13
 
 divisibilityTestBy :: Int -> Int -> Bool
 divisibilityTestBy 2 2    = True
@@ -27,7 +30,6 @@ divisibilityTestBy 7 7    = True
 divisibilityTestBy x 2  = (x `mod` 10) `elem` [0, 2, 4, 6, 8]
 divisibilityTestBy x 3  = if x `div` 10 == 0 then x `elem` [3, 6, 9] else sum (digits x) `divisibilityTestBy` 3
 divisibilityTestBy x 5  = (x `mod` 10) `elem` [0, 5]
-divisibilityTestBy x 10 = (x `mod` 10) == 0
 -- NOTE: Not foolproof divisibility tests
 divisibilityTestBy x 11 = let d = digits x in length d == 2 && head d == d !! 1
 divisibilityTestBy _ _ = False
@@ -49,21 +51,21 @@ primes = sieve [2..]
     sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
 
 isPrimeLooking :: Int -> Bool
-isPrimeLooking n = not (passesDivisibilityTests n) && not (isSquare n)
+isPrimeLooking n = n `elem` [2, 3, 5, 7, 11, 13, 17, 19] || (not (passesDivisibilityTests n) && not (isSquare n))
   where
     passesDivisibilityTests = getAny . foldMap (Any .: flip divisibilityTestBy) allDivisibilityTests
 
 
-sortedDiff :: Ord a => [a] -> [a] -> [a]
-sortedDiff (x:xs) (y:ys) = case x `compare` y of
-  LT -> x : sortedDiff xs (y:ys)
-  EQ -> sortedDiff xs ys
-  GT -> y : sortedDiff (x:xs) ys
-sortedDiff [] ys = ys
-sortedDiff xs [] = xs
+leftSortedDiff :: Ord a => [a] -> [a] -> [a]
+leftSortedDiff (x:xs) (y:ys) = case x `compare` y of
+  LT -> x : leftSortedDiff xs (y:ys)
+  EQ -> leftSortedDiff xs ys
+  GT -> leftSortedDiff (x:xs) ys
+leftSortedDiff [] ys = ys
+leftSortedDiff xs [] = xs
 
-realPrimes = takeWhile (<= lockers) $ dropWhile (<= 20) primes
-lookingPrime = filter isPrimeLooking [20..lockers]
+realPrimes = takeWhile (<= lockers) $ primes
+lookingPrime = filter isPrimeLooking [1..lockers]
 
 powersOfTwo = takeWhile (<= lockers) $ concatMap (\x -> [x-1, x]) $ iterate (2*) 2
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
@@ -71,7 +73,38 @@ fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 main :: IO ()
 main = do
   forM_ foolproofDivisibilityTests test
-  print $ sortedDiff realPrimes lookingPrime
+  putStrLn $ "Left out primes: " ++ (show $ leftSortedDiff realPrimes lookingPrime)
+  putStrLn $ "Fake primes: " ++ (show $ leftSortedDiff lookingPrime realPrimes)
   putStrLn $ "Available lockers using 2-powers: " ++ show (length powersOfTwo)
   putStrLn $ "Available lockers using fibs: " ++ show (length $ takeWhile (<= lockers) fibs)
   putStrLn $ "Available lockers using looking like primes: " ++ show (length lookingPrime)
+  putStrLn $ "Available lockers using primes: " ++ show (length realPrimes)
+
+
+  forM_ (leftSortedDiff lookingPrime realPrimes) $ \n ->
+      putStrLn (show n ++ " " ++ (show $ filter (\x -> n `mod` x == 0) realPrimes))
+
+count = sum . map (bool 0 1)
+
+-- https://en.wikipedia.org/wiki/Precision_and_recall#Definition
+confusion t p = length $ filter (\n -> elem n realPrimes == t && elem n lookingPrime == p) [1..lockers]
+
+tp, fp, tn, fn :: Float
+tp = fromIntegral $ confusion True  True
+fp = fromIntegral $ confusion False True
+tn = fromIntegral $ confusion False False
+fn = fromIntegral $ confusion True  False
+
+accuraccy, precision, recall, f1 :: String
+accuraccy = printf "%.2f" $ (tp + tn) / 230
+precision = printf "%.2f" $ tp / fromIntegral (length lookingPrime)
+recall    = printf "%.2f" $ tp / fromIntegral (length realPrimes)
+f1        = printf "%.2f" $ 2 * tp / (2 * (tp + fp + fn))
+
+
+table = "<tr>" ++ (concat $ intersperse "\n</tr><tr>\n" $ map (\n -> "<td>" ++ show n ++ "</td>" ++ (toHtml $ primeDivisors n)) fakes) ++ "</tr>"
+  where
+    fakes = leftSortedDiff lookingPrime realPrimes
+    primeDivisors n = filter (\x -> n `mod` x == 0) realPrimes
+    toMask divisors = map (\prime -> prime `elem` divisors) [7,11,13,17,19,23,29,31]
+    toHtml divisors = concat $ map (bool "<td class=\"false\">F</td>" "<td class=\"true\">T</td>") $ toMask divisors
