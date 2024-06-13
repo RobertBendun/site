cd1e977a6e63aa9e43d8210128764cbb63bccf54
Robert Bendun <robert@bendun.cc>
2024-06-13 02:09:31 +0200

    Furiosa

--- cinema/posters/fix-size.sh
+++ cinema/posters/fix-size.sh
@@ -6,6 +6,6 @@ for fname in *.jpg; do
 	if [ ! -f backups/$fname ]; then
 		echo "Resizing $fname"
 		mv {,backups/}$fname
-		convert backups/$fname -resize x150 $fname
+		magick backups/$fname -resize x150 $fname
 	fi
 done
