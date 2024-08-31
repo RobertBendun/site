534e98efbd60ae6afb67ae8813ad30930a0d9619
Robert Bendun <robert@bendun.cc>
2024-08-31 22:06:42 +0200

    New relative dates subscripts mechanism and post about it

--- archive/index.py
+++ archive/index.py
@@ -124,6 +124,7 @@ class Tags:
 
 
 POSTS = sorted([
+    Post(date="2024-08-31", path="years-ago.html", name="Trouble with Dates"),
     Post(date="2024-08-26", path="phoneless.html", name="Phoneless"),
     Post(date="2024-08-18", path="horror.html", name="Horror"),
     Post(date="2024-06-19", path="output.html", name="On Output"),
