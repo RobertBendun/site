06e9abd88add5705628a6f5be5fdb7772ece6791
Robert Bendun <robert@bendun.cc>
2024-12-29 12:41:42 +0100

    Sitcoms

--- db/gen.cc
+++ db/gen.cc
@@ -1,5 +1,7 @@
 #include <cassert>
 #include <cctype>
+#include <cstring>
+#include <fstream>
 #include <set>
 #include <sys/queue.h>
 #include <string_view>
@@ -14,6 +16,7 @@
 #include <algorithm>
 #include <print>
 #include <generator>
+#include <map>
 
 #include <lowdown.h>
 
@@ -54,17 +57,20 @@ static_assert(std::forward_iterator<inclusive_range<int>>);
 static_assert(std::ranges::forward_range<inclusive_range<int>>);
 
 using Seasons = std::vector<inclusive_range<std::chrono::year>>;
+using Watched = std::map<std::size_t, Date>;
 
 struct Series
 {
 	std::string path;
 	std::string_view title;
 	std::string_view review;
+	std::string_view network;
 
-	std::string_view watched, network;
-
+	Watched watched;
 	Seasons seasons;
 	std::vector<std::string> tags;
+	std::size_t score;
+	bool musical_episode = false;
 
 	static Series from_file(char const* path);
 	void dump();
@@ -95,7 +101,7 @@ Seasons parse_seasons_declaration(std::string_view s)
 	enum {
 		SEASON_NUMBER,
 		YEAR_RANGE,
-	} state;
+	} state = SEASON_NUMBER;
 
 	Seasons seasons;
 
@@ -135,6 +141,63 @@ Seasons parse_seasons_declaration(std::string_view s)
 	return seasons;
 }
 
+Watched parse_watched_declaration(std::string_view s) {
+	std::size_t number;
+	enum {
+		SEASON_RANGE,
+		YEAR_MONTH,
+	} state = SEASON_RANGE;
+
+	Watched watched;
+
+	inclusive_range<std::size_t> season_range;
+
+	while (s.size()) {
+		while (s.size() && std::isspace(s.front())) s.remove_prefix(1);
+		if (s.empty()) break;
+
+		auto [p, ec] = std::from_chars(s.data(), s.data() + s.size(), number);
+		assert(ec == std::errc{}); // TODO: Implement better error handling
+		s = { p, s.data() + s.size() };
+
+		switch (state) {
+		break; case SEASON_RANGE: {
+			season_range.from = season_range.to = number;
+			if (s.starts_with("..")) {
+				s.remove_prefix(2);
+				auto [p, ec] = std::from_chars(s.data(), s.data() + s.size(), number);
+				assert(ec == std::errc{}); // TODO: Implement better error handling
+				s = { p, s.data() + s.size() };
+				season_range.to = number;
+			}
+			state = YEAR_MONTH;
+		}
+		break; case YEAR_MONTH: {
+			Date date;
+			date.year = std::chrono::year(number);
+
+			if (s.starts_with("-")) {
+				s.remove_prefix(1);
+				auto [p, ec] = std::from_chars(s.data(), s.data() + s.size(), number);
+				assert(ec == std::errc{}); // TODO: Implement better error handling
+				s = { p, s.data() + s.size() };
+				date.month = std::chrono::month(number);
+			}
+
+			for (auto season : season_range) {
+				watched[season] = date;
+			}
+
+			state = SEASON_RANGE;
+		}
+		}
+	}
+
+	assert(state == SEASON_RANGE);
+
+	return watched;
+}
+
 Series Series::from_file(char const* path)
 {
 	Series series;
@@ -172,11 +235,14 @@ Series Series::from_file(char const* path)
 				series.tags.push_back(next);
 			}
 		}
-		else if (key == "watched") { series.watched = m->value; }
-		else if (key == "seasons") {
-			series.seasons = parse_seasons_declaration(m->value);
-		}
+		else if (key == "watched") { series.watched = parse_watched_declaration(m->value); }
+		else if (key == "seasons") { series.seasons = parse_seasons_declaration(m->value); }
 		else if (key == "network") { series.network = m->value; }
+		else if (key == "musical-episode") { series.musical_episode = true; }
+		else if (key == "score") {
+			auto [p, ec] = std::from_chars(m->value, m->value + std::strlen(m->value), series.score);
+			assert(ec == std::errc{});
+		}
 		else {
 			std::cerr << program_name << ": error: " << path << ": unrecognized metadata key: " << m->key << "\n";
 			continue;
@@ -220,28 +286,111 @@ Series_Collection Series_Collection::from_directory(std::filesystem::path root)
 	return s;
 }
 
+std::string pretty_print_watched(Series const& series)
+{
+	size_t last_season_watched = 0;
+	std::chrono::year finished_watching_in;
+
+	for (auto [season_number, date] : series.watched) {
+		last_season_watched = std::max(last_season_watched, season_number);
+		finished_watching_in = date.year;
+	}
+
+	if (last_season_watched == series.seasons.size()) {
+		return std::format("finished in {}", finished_watching_in);
+	} else {
+		return std::format("stopped at season {} in {}", last_season_watched, finished_watching_in);
+	}
+}
+
 int main(int, char **argv)
 {
 	program_name = std::filesystem::path(argv[0]).filename().string();
 
 	auto coll = Series_Collection::from_directory("./series/");
 
+	std::ofstream sitcoms_output_file{"sitcoms.html", std::ios::trunc|std::ios::out};
 
-	auto out = [](auto &&value) {
-		std::cout << value;
+	auto out = [&sitcoms_output_file](auto &&value) {
+		sitcoms_output_file << value;
 	};
 
 	auto [sitcom_begin, sitcom_end] = coll.tags.equal_range("sitcom");
 	auto sitcoms = std::ranges::subrange(sitcom_begin, sitcom_end)
 			| std::views::transform([](auto &p) -> auto& { return p.second; })
 			| std::ranges::to<std::vector>();
-	std::ranges::sort(sitcoms, std::less<>{}, [](auto const& x) -> auto& { return *x; });
+	std::ranges::sort(sitcoms, std::less<>{}, [](auto const& x) -> auto const& { return *x; });
 
 	auto const [year_start, year_end] = std::ranges::minmax(
 			sitcoms
 		| std::views::transform([](auto const& s) { return s->seasons; })
 		| std::views::join
 		| std::views::join);
+
+
+	std::map<std::chrono::year, std::size_t> sitcoms_in_each_year;
+	for (auto year : sitcoms
+			| std::views::transform([](auto const& s) { return s->bounds(); })
+			| std::views::join
+	) {
+		++sitcoms_in_each_year[year];
+	}
+	auto years_with_sitcom_count = std::move(sitcoms_in_each_year) | std::ranges::to<std::vector>();
+	assert(std::ranges::is_sorted(years_with_sitcom_count));
+	auto year_with_most_sitcoms = std::ranges::max_element(years_with_sitcom_count, [](auto const& l, auto const& r) { return l.second < r.second; });
+
+
+	auto years_with_most_sitcoms_range = std::ranges::subrange{year_with_most_sitcoms, years_with_sitcom_count.end()}
+			| std::views::take_while([&](auto const& p) { return p.second == year_with_most_sitcoms->second; })
+			| std::views::enumerate;
+
+	std::string years_with_most_sitcoms;
+	{
+		auto count = std::ranges::distance(std::ranges::begin(years_with_most_sitcoms_range), std::ranges::end(years_with_most_sitcoms_range));
+		for (auto [i, x] : years_with_most_sitcoms_range) {
+			if (i != 0) {
+				if (i+1 == count) {
+					years_with_most_sitcoms += " and ";
+				} else {
+					years_with_most_sitcoms += ", ";
+				}
+			}
+			years_with_most_sitcoms += std::format("{}", x.first);
+		}
+	}
+	auto const years_count = (year_end - year_start).count();
+
+	std::map<std::chrono::year, std::size_t> seasons_watched_in_year;
+	std::map<std::chrono::year, std::size_t> sitcoms_watched_in_year;
+
+	for (auto const& sitcom : sitcoms) {
+		auto const years = sitcom->watched | std::views::values | std::views::transform(&Date::year);
+		for (auto year : years | std::ranges::to<std::set>()) {
+			sitcoms_watched_in_year[year]++;
+		}
+		for (auto year : years) {
+			seasons_watched_in_year[year]++;
+		}
+	}
+
+	auto shows_with_musical_episodes = sitcoms
+		| std::views::filter([](auto const& s) { return s->musical_episode; })
+		| std::views::transform([](auto const& s) { return s->title; })
+		| std::ranges::to<std::vector>();
+
+	auto shows_with_musical_episodes_list = std::string{};
+
+	for (auto [i, x] : std::views::enumerate(shows_with_musical_episodes)) {
+			if (i != 0) {
+				if (i+1 == std::ranges::ssize(shows_with_musical_episodes)) {
+					shows_with_musical_episodes_list += " and ";
+				} else {
+					shows_with_musical_episodes_list += ", ";
+				}
+			}
+			shows_with_musical_episodes_list += x;
+		}
+
 
 #include "sitcoms.hh"
 
