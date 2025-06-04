2ff840ff1325387f9c379c3ff4a05938120fab92
Robert Bendun <robert@bendun.cc>
2025-06-04 15:56:01 +0200

    backlog: Week with Java

--- archive/index.py
+++ archive/index.py
@@ -178,6 +178,7 @@ class Tags:
 
 
 POSTS = sorted([
+    Post(path="week-with-java.html", name="Week with Java"),
     Post(path="what-i-watched-in-april-2025.html", name="What I Watched in April"),
     Post(path="residue-numbers.html", name="Residue Number System"),
     Post(path="what-i-watched-in-march-2025.html", name="What I Watched in March"),
